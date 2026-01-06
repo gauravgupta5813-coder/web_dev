0f2e213d2f57115add67671ec254dc7d7e112432
gauravgupta5813-coder <gauravgupta5813@gmail.com>
2026-01-07 00:35:19 +0530

    version 2

--- Javascript/hello.js
+++ Javascript/hello.js
@@ -2,7 +2,15 @@ function printChai(){
     console.log("Hello Chai")
 }
 
-function bringBrush(){
-    
+function bringBrush(itne){
+    console.log(`Hanji, le aaya ${itne} brush`);
 }
-printChai()
+
+function addTwo(ekNum, doNum){
+    return ekNum + doNum;
+}
+
+bringBrush(14);
+printChai();
+addTwo(1,1);
+console.log(addTwo(3,2));
