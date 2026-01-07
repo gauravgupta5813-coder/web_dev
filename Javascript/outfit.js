1c6b35830f5bf2fb11f9dfedc120f5df02033edf
gauravgupta5813-coder <gauravgupta5813@gmail.com>
2026-01-07 22:27:36 +0530

    adding examples

---
+++ Javascript/outfit.js
@@ -0,0 +1,19 @@
+// server call
+
+let weather = "rainy"
+
+// if (){
+//     //
+// }else {
+//     //
+// }
+
+if (weather === "cloudy"){
+   console.log("jacket le jaa bhai");
+} else if (weather === "rainy"){
+    console.log("it's raining, carry umbrella");
+} else {
+    console.log("sunny h ji aaj toh");
+}
+
+console.log(weather === "rainy");
