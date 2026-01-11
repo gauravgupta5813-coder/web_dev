6575f560ea0260fb4630ab257a7754442eb35032
gauravgupta5813-coder <gauravgupta5813@gmail.com>
2026-01-11 13:21:58 +0530

    Adding objects function

--- Javascript/JS - OBJECTS/index.js
+++ Javascript/JS - OBJECTS/index.js
@@ -17,3 +17,36 @@ const person = {
 }
 
 console.log(person.address)
+
+let p1 = {
+    fname: 'Hitesh',
+    lname: 'Ch',
+    address: {
+       h: 1,
+       s: 1
+    }
+}
+
+const p1KaString = JSON.stringify(p1)
+console.log(p1KaString)
+let p2 = JSON.parse(p1KaString)
+
+
+// let p2 = {
+//     fname: p1.fname,
+//     lname: p1.lname,
+//     address: p1.address  
+// }
+
+// let p2 = {
+//     ...p1,  //shallow copy //spread operator
+//     address: {
+//         ...p1.address
+//     }
+// }
+
+p2.fname = 'Piyush Garg'
+p2.address.h = 'Hacked'
+
+console.log(p2)
+console.log(p1) 
